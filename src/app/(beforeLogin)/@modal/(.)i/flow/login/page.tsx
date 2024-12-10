dcdf22129ce37893940be530beda5c4a4b5e9e7c
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-11 00:46:06 +0900

    Login에서 Redirection수정 (오류 해결)

--- src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
@@ -1,21 +1,8 @@
-"use client";
+import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
 
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import Main from "@/app/(beforeLogin)/_component/Main";
+export default function Page() {
 
-export default function Login() {
-  const router = useRouter();
-
-  useEffect(()=>{
-    router.replace('/i/flow/login');
-  },[router]);
-  
-  return <Main/>;
+  return (
+    <LoginModal />
+  );
 }
-
-//router.push
-// localhost:3001/login -> localhost:3001/login -> localhost:3001/i/flow/login
-// router.push를 했을 때 문제는 뒤로가기 했을 때 localhost:3001/login으로 돌아감 (i/flow/login으로 돌아오는 현상 반복)
-
-// router
--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,22 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import Main from "../_component/Main";
 
 export default function Login() {
-  redirect('/i/flow/login');  
+  const router = useRouter();
+
+  useEffect(()=>{
+    router.replace('/i/flow/login');
+  },[router]);
+  
+  return <Main/>;
 }
+
+
+// import { redirect } from "next/navigation";
+
+// export default function Login() {
+//   redirect('/i/flow/login');  
+// }
